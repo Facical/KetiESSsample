e54379eeeb24d8b6fca7e6671d36562b9051bd90
Facical <joh0816@gamil.com>
2025-07-31 16:14:31 +0900

    Initial Commit

---
+++ KetiESSsample/KetiESSsampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KetiESSsampleApp.swift
+//  KetiESSsample
+//
+//  Created by . on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct KetiESSsampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
